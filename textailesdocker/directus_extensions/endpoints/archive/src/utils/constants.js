68547095078474a3b6c19bdc470960d70da75290
Thomas Pappas <getekid@gmail.com>
2025-12-18 04:47:15 +0200

    Adds Lexend Deca google font styles and sets it as body default

--- textailesdocker/directus_extensions/endpoints/archive/src/utils/constants.js
+++ textailesdocker/directus_extensions/endpoints/archive/src/utils/constants.js
@@ -26,7 +26,7 @@ export const PATHS = {
 // Content Security Policy for the web pages
 // Allows inline scripts and styles needed for the UI to work
 // blob: is needed for model-viewer to load embedded textures from GLB files
-export const CSP_POLICY = "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://ajax.googleapis.com; script-src-elem 'self' 'unsafe-inline' https://ajax.googleapis.com https://cdnjs.cloudflare.com; connect-src 'self' https://ajax.googleapis.com https://www.gstatic.com blob:; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; font-src 'self' https://cdnjs.cloudflare.com; img-src 'self' data: blob:; worker-src 'self' blob:; child-src 'self' blob:;";
+export const CSP_POLICY = "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://ajax.googleapis.com; script-src-elem 'self' 'unsafe-inline' https://ajax.googleapis.com https://cdnjs.cloudflare.com; connect-src 'self' https://ajax.googleapis.com https://www.gstatic.com blob:; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com; img-src 'self' data: blob:; worker-src 'self' blob:; child-src 'self' blob:;";
 
 // ATON Configuration - REST API v2
 export const ATON_CONFIG = {
