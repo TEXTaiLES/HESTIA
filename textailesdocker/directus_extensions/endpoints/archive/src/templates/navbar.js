5737ac8a37c8581f52d781410d53cdcc955d3e5a
Thomas Pappas <getekid@gmail.com>
2025-12-23 01:51:15 +0200

    Fixes mobile header styles, developed by @Xenobii

--- textailesdocker/directus_extensions/endpoints/archive/src/templates/navbar.js
+++ textailesdocker/directus_extensions/endpoints/archive/src/templates/navbar.js
@@ -32,34 +32,47 @@ export const renderNavbar = (activePage = 'home') => {
 
 <nav id="menu" class="ntnavbar navbar-expand-lg" aria-label="navbar">
     <div class="container">
-        <div class="row mt-1">
-            <div class="col-md-2 col-sm-12">
-                <a class="navbar-brand" href="/archive">
-                    <img src="/archive/static/Logos/Logo-Textailes_Logo-Textailes-Colour-RGB-Hor.svg" alt="TEXTaiLES logo"><span>Portal</span>
+        <!-- Top row: logo + burger -->
+        <div class="row align-items-center mt-1">
+            <!-- Logo -->
+            <div class="col-8 col-lg-2">
+                <a class="navbar-brand d-flex align-items-center" href="/archive">
+                    <img src="/archive/static/Logos/Logo-Textailes_Logo-Textailes-Colour-RGB-Hor.svg" alt="TEXTaiLES logo" class="img-fluid">
+                    <span>Portal</span>
                 </a>
             </div>
-            <div class="col-md-10 col-sm-12 text-end">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
-                    <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+            <!-- Burger -->
+            <div class="col-4 col-lg-10 d-flex justify-content-end align-items-center">
+                <button class="navbar-toggler d-flex align-items-center justify-content-center d-lg-none" 
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarMenu"
+                        aria-controls="navbarMenu"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <i class="fas fa-bars d-block"></i>
                 </button>
-                <div class="collapse navbar-collapse horizontal_border" id="navbarMenu">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 nt_mtop text-uppercase">
-                        <li class="nav-item">
-                            <a class="nav-link py-1 ${isActive('home')}" href="/archive">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-1 ${isActive('collections')}" href="/archive/collections">Collections</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-1 ${isActive('toolbox')}" href="/archive/toolbox">Toolbox</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex align-items-center pb-5">
-                        <a href="/admin/login" class="text-decoration-none">
-                            <img src="/archive/static/Icons/header_login_icon.png" alt="Login" style="height: 30px;">
-                        </a>
-                    </div>
-                </div>
+            </div>
+        </div>
+        <!-- Collapsible menu -->
+        <div class="collapse navbar-collapse horizontal_border" id="navbarMenu">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nt_mtop text-uppercase text-center text-lg-start">
+                <li class="nav-item">
+                    <a class="nav-link py-1 ${isActive('home')}" href="/archive">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link py-1 ${isActive('collections')}" href="/archive/collections">Collections</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link py-1 ${isActive('toolbox')}" href="/archive/toolbox">Toolbox</a>
+                </li>
+            </ul>
+
+            <!-- User icon -->
+            <div class="d-flex justify-content-center justify-content-lg-end pb-3 pb-lg-0">
+                <a href="/admin/login" class="text-decoration-none">
+                    <img src="/archive/static/Icons/header_login_icon.png" alt="Login" style="height: 30px;">
+                </a>
             </div>
         </div>
     </div>
