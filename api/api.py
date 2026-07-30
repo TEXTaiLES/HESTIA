0f3d4c9a9edb75ffa4bc596108229d9ff51cc00d
Ioannis Giannoukos <ioannhsyan76@gmail.com>
2026-07-30 14:36:14 +0300

    Removes the duplicate nefele_job import

--- api/api.py
+++ api/api.py
@@ -21,7 +21,6 @@ from resources.amalthai_dataset import AmalthaiDatasetResource, AmalthaiDatasetI
 from resources.amalthai_model import AmalthaiModelResource, AmalthaiModelItemResource, AmalthaiModelWeightsResource, AmalthaiModelConfigResource
 from resources.amalthai_experiment import AmalthaiExperimentResource, AmalthaiExperimentItemResource
 from resources.amalthai_inference import AmalthaiInferenceResource, AmalthaiInferenceItemResource, AmalthaiInferenceInputsResource, AmalthaiInferenceOutputsResource
-from resources.nefele_job import NefeleResource, NefeleJobResource, NefeleClaimResource, NefelePreviewResource
 from resources.multispectral import (
     MultispectralImageFileResource,
     MultispectralImageListResource,
